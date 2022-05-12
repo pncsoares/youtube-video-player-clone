1b559f5583969e991c7e77e2dbdfadbc38b83fe3
Pedro Soares <pncsoares@gmail.com>
2022-05-12 08:33:17 +0100

    Toggle play/pause when we click space, k or we click in the video frame

--- src/script.js
+++ src/script.js
@@ -2,7 +2,17 @@ const playPauseBtn = document.querySelector('.play-pause-btn');
 const videoContainer = document.querySelector('.video-container');
 const video = document.querySelector('video');
 
+document.addEventListener('keydown', e => {
+    switch (e.key.toLowerCase()) {
+        case ' ':
+        case 'k':
+            togglePlayPause();
+            break;
+    }
+});
+
 playPauseBtn.addEventListener('click', togglePlayPause);
+video.addEventListener('click', togglePlayPause);
 
 function togglePlayPause() {
     video.paused ? video.play() : video.pause();
